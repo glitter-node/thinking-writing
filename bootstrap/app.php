2fea9fd3faa906713504d55d11d891afa773a942
glitter gim <gim@glitter.kr>
2026-03-13 14:27:31 +0900

    fix: google one tap login

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,9 +1,7 @@
 <?php
 
-use App\Http\Middleware\ContentSecurityPolicy;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
-use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -11,11 +9,7 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->web(append: [
-            ContentSecurityPolicy::class,
-        ]);
-    })
+    ->withMiddleware(function (): void {})
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
